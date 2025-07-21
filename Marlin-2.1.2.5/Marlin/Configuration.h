376c957950bcf60fc56c4f93b5473c4b41138fbe
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-07-21 18:08:51 +0200

    Merge branch 'Feature/konfiguracja-odleglosci' into Development
    (cherry picked from commit c17267b01eedbbed92fe9d34ae6824ce605dd8e6)

--- Marlin-2.1.2.5/Marlin/Configuration.h
+++ Marlin-2.1.2.5/Marlin/Configuration.h
@@ -1513,7 +1513,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 40, 0, 2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1731,7 +1731,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -5
+#define Y_MIN_POS -25
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
