8cfe29320c0f8fb181ded935f261ee3fc1a19770
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-09-02 19:33:32 +0200

    dokładniejsze offsety

--- Marlin-2.1.2.5/Marlin/Configuration.h
+++ Marlin-2.1.2.5/Marlin/Configuration.h
@@ -1513,7 +1513,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40, 0, 2 }
+#define NOZZLE_TO_PROBE_OFFSET { 41, 0, -2.2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1726,12 +1726,12 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 270
+#define X_BED_SIZE 200
+#define Y_BED_SIZE 259
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS -25
+#define X_MIN_POS -3
+#define Y_MIN_POS -24
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
