c74bf7e7d04a250a0fd44494fb89be99afff845e
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-09-17 17:49:48 +0200

    więcej grid points

--- Marlin-2.1.2.5/Marlin/Configuration.h
+++ Marlin-2.1.2.5/Marlin/Configuration.h
@@ -1994,7 +1994,7 @@
 
   // Set the number of grid points per dimension.
   #define GRID_MAX_POINTS_X 6
-  #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
+  #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X+2
 
   // Probe along the Y axis, advancing X after each column
   //#define PROBE_Y_FIRST
